7fb685f757707c08b5f23bf2678d63373d9b697c
deepanjii <deepanjoan95@gmail.com>
2022-06-20 22:31:03 +0530

    fixed webpack prod config

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -12,7 +12,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.m?js$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
@@ -35,5 +35,8 @@ module.exports = {
       inject: true,
       template: "./public/index.html",
     }),
-  ]
+  ],
+  resolve: {
+    extensions: [".js", ".jsx"],
+  }
 };
